432a8a1b46717857fc1466117c457dfc5b44f372
rilian <dmitriyis@gmail.com>
2012-10-05 20:28:37 +0300

    seller

--- sellerd.rb
+++ sellerd.rb
@@ -13,10 +13,24 @@ require 'randomizer'
 
 @logger = Logger.new(STDOUT)
 @seller = DaemonMarket::Seller.new(store: DaemonMarket::Randomizer.random_list, account: {pin: 1234, id: 'seller_1', money: 100 })
+@seller_id = 'seller_1'
 
 EventMachine.run do
 
   @logger.info "Started seller #{@seller.account.id} with #{@seller.store.inspect}"
+
+  AMQP.start("amqp://guest:guest@192.168.1.130/dima") do |connection|
+    @connection = connection
+
+    channel = AMQP::Channel.new(@connection)
+
+    market_ex = channel.topic("market", auto_delete: true)
+
+    10.times do
+      market_ex.publish(@seller.announce, routing_key: "market.ads.#{@seller_id}")
+    end
+
+  end
 
   on_exit_signal = Proc.new do
     @logger.info "Closing connections..."
